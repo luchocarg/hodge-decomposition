5d56279b483d79bf51fe48516f6dc44cabb7d741
luchocarg <thebulletofcharly@gmail.com>
2026-02-03 23:58:40 -0300

    Refactor Engine

--- Engine/src/Main.hs
+++ Engine/src/Main.hs
@@ -1,30 +1,36 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
-import qualified Data.ByteString.Lazy as ByteString
+import Foreign.C.String (CString, newCString, peekCString)
+import Foreign.Ptr (Ptr)
+import qualified Data.ByteString.Lazy.Char8 as BS
 import Data.Aeson (decode, encode)
-import System.IO (hPutStrLn, stderr)
 
 import qualified Infrastructure.JsonDto as Dto
 import qualified Infrastructure.Mappers as Mapper
-
 import qualified Domain.Types as Dom
 import qualified ContinuousMath.Decomposition as Logic
 
-main :: IO ()
-main = do
-    inputData <- ByteString.getContents
-    
-    case decode inputData :: Maybe Dto.IncomingGraphDto of
-        Nothing -> 
-            hPutStrLn stderr "Error: Invalid JSON Format. Could not parse input graph."
-            
-        Just incomingDto -> do
-            let domainGraph = Mapper.toComputationalGraph incomingDto
+foreign export ccall "run_decomposition" run_decomposition :: CString -> IO CString
 
-            let simulationResult = Logic.decompose domainGraph
+run_decomposition :: CString -> IO CString
+run_decomposition inputPtr = do
+    jsonStr <- peekCString inputPtr
+    let inputData = BS.pack jsonStr
+    
+    let outputData = case decode inputData :: Maybe Dto.IncomingGraphDto of
+            Nothing -> 
+                encode $ Dto.IncomingGraphDto [] []
             
-            let outgoingDto = Mapper.toOutgoingDto simulationResult
-            
-            ByteString.putStr (encode outgoingDto)
+            Just incomingDto -> 
+                let domainGraph = Mapper.toComputationalGraph incomingDto
+                    simulationResult = Logic.decompose domainGraph
+                    outgoingDto = Mapper.toOutgoingDto simulationResult
+                in encode outgoingDto
+
+    newCString (BS.unpack outputData)
+
+main :: IO ()
+main = return ()
